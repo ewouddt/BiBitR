8f4c1146e028a6aa0c4fae2b6b549911d14d57cb
Ewoud De Troyer <ewoud.detroyer@uhasselt.be>
2016-11-09 23:16:51 +0100

    update R documentation

--- R/bibit.R
+++ R/bibit.R
@@ -350,6 +350,8 @@ MaxBC <- function(bicresult,top=1){
 #' data <- data[sample(1:nrow(data),nrow(data)),sample(1:ncol(data),ncol(data))]
 #' 
 #' make_arff_row_col(matrix=data,name="data",path="")
+#' 
+#' result <- bibit(data,minr=5,minc=5,arff_row_col=c("data_arff.arff","data_rownames.csv","data_colnames.csv"))
 #' }
 make_arff_row_col <- function(matrix,name="data",path=""){
   if(class(matrix)!="matrix"){stop("matrix parameter should contain a matrix object",call.=FALSE)}
